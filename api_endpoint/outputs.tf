0cd0af9ec81eaa45d0a6eda5265d41627c426222
jnienhui <jnienhuis132@gmail.com>
2024-09-24 12:04:53 -0600

    fix

--- api_endpoint/outputs.tf
+++ api_endpoint/outputs.tf
@@ -1,4 +1,4 @@
 output "api_endpoint_lambda_arns" {
     description = "The ARNs of the Lambda functions from api_endpoint"
-    value       = { for method in module.endpoint_methods : method.key => method.lambda_arn }
+    value       = { for method_key, method in module.endpoint_methods : method_key => method.lambda_arn }
 }
