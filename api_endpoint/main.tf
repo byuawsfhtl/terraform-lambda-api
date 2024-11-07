93cda9ccd853475a6f967d7534cf0fa1f5540053
Hunter Corry <102935249+hcorry98@users.noreply.github.com>
2024-11-07 12:29:19 -0700

    Added concurrent executions variable (#9)

--- api_endpoint/main.tf
+++ api_endpoint/main.tf
@@ -1,9 +1,10 @@
 locals {
   method_map = {
     for def in var.method_definitions : "${var.app_name}_${var.path_part}_${def.http_method}" => {
-      http_method = def.http_method
-      command     = def.command
-      timeout     = def.timeout
+      http_method                    = def.http_method
+      command                        = def.command
+      timeout                        = def.timeout
+      reserved_concurrent_executions = def.reserved_concurrent_executions
     }
   }
 
@@ -32,9 +33,10 @@ module "endpoint_methods" {
   lambda_role_arn              = var.lambda_role_arn
   path_part                    = var.path_part
 
-  http_method = each.value.http_method
-  command     = each.value.command
-  timeout     = each.value.timeout
+  http_method                    = each.value.http_method
+  command                        = each.value.command
+  timeout                        = each.value.timeout
+  reserved_concurrent_executions = each.value.reserved_concurrent_executions
 
   api_gateway     = var.api_gateway
   api_resource_id = aws_api_gateway_resource.api_resource.id
