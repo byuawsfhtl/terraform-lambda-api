c85206ee9731219161262e95f93d2303273a190d
Thomas Bean <47thomasj@gmail.com>
2026-01-16 16:38:20 -0700

    outputed domain_name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "lambda_api_lambda_arns" {
   description = "The ARNs of the Lambda functions from all api_endpoint instances"
   value       = { for api_key, api in module.api_endpoint : api_key => api.api_endpoint_lambda_arns }
 }
+
+output "api_gateway_domain_name" {
+  value = aws_api_gateway_domain_name.api_gateway_domain_name
+}
